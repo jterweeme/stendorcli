b357a07a32d611827b4d04f047f86d9e47fa93c8
Jasper ter Weeme <jasper@nbweeme.lan>
2013-05-15 20:12:13 +0200

    no message this time

--- src/com/stendor/stendorcli/Main.java
+++ src/com/stendor/stendorcli/Main.java
@@ -2,6 +2,11 @@ package com.stendor.stendorcli;
 
 public class Main extends android.app.Activity
 {
+    protected void onCreate(android.os.Bundle sis)
+    {
+        super.onCreate(sis);
+        setContentView(R.layout.main);
+    }
 }
 
 
